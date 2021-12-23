a10a1a0b5def44d493a163d9009d1eb1e33d4ad0
Hanish565 <74765868+Hanish565@users.noreply.github.com>
2021-12-23 11:40:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,15 +15,22 @@ function draw(){
 
    for ( i = 0; i < objects.length; i++) {
       document.getElementById("status").innerHTML="Status:Object Detected";
-      document.getElementById("numberofobjects").innerHTML= "The Number of Objects Detected:" + objects.length;
       fill(red,green,blue);
       percentage=floor(objects[i].confidence*100)
       text(objects[i].label + " " + percentage + " % ",objects[i].x+30,objects[i].y+30);
       noFill();
-      stroke(red,green,blue);
+      stroke("red");
       rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
       console.log(objects.length);
+      if (input==objects[i].label) {
+      video.stop();   
+      document.getElementById("object").innerHTML= input + " Has been found ";
+      objectDetector.detect(video,gotResults);
+      
+
+          }
    }
+  
   }
 }
 
@@ -33,11 +40,6 @@ function setup(){
     video=createCapture(VIDEO);
     video.size(380,380);
     video.hide();
-    //objectDetector=ml5.objectDetector("cocossd", modeloaded);
-    document.getElementById("status").innerHTML="Status:Detecting Object";
-    red=Math.floor(Math.random()*256);
-    green=Math.floor(Math.random()*256);
-    blue=Math.floor(Math.random()*256);
 }
 
 function modeloaded(){
@@ -45,11 +47,18 @@ function modeloaded(){
   status=true;
 }
 
+function start(){
+  objectDetector=ml5.objectDetector("cocossd", modeloaded);
+  document.getElementById("status").innerHTML="Status:Detecting Object";
+  input=document.getElementById("text_input").value;
+}
+
 function gotResults(error,results){
   if (error) {
     console.error(error);
-  }  
+  }
+  else  {
     console.log(results);
     objects=results;
-  
+  }
 }
